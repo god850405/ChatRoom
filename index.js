2f69e39423b5454e3fede5a243dc3fc320677943
marcus.chen <god850405@gmail.com>
2021-08-28 01:18:41 +0800

    調整環境與埠號

--- index.js
+++ index.js
@@ -3,42 +3,37 @@ var fs = require('fs')
     // 如果不需要用 https 的話，要改成引用 http 喔
 var http = require('http')
 var socketio = require('socket.io')
+var app = require('express')();
 
 //https 的一些設定，如果不需要使用 ssl 加密連線的話，把內容註解掉就好
 var options = {
     // key: fs.readFileSync('這個網域的 ssl key 位置'),
     // cert: fs.readFileSync('這個網域的 ssl fullchain 位置')
 }
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
 
 //http & socket port
-var server = http.createServer(options);
-server.listen(4040)
+var server = http.createServer(app); //option
+server.listen(PORT)
 var io = socketio(server, {
     cors: {
         origin: "*",
         credentials: true
     }
 });
-console.log("Server socket 4040 , api 4000")
+app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    //  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-//api port
-var app = require('express')();
-var port = 4000;
-app.listen(port, function() {
-    console.log('API listening on *:' + port);
-});
-
-const INDEX = __dirname + '/index.html';
-
-//用 api 方式取得
-app.get('/', function(req, res) {
-    res.sendFile(INDEX);
-    // let messages = 'hellow world'
-    // res.send(messages);
-})
+// //用 api 方式取得
+// app.get('/', function(req, res) {
+//     res.sendFile(INDEX);
+//     // let messages = 'hellow world'
+//     // res.send(messages);
+// })
 
 var messages = [
-    { name: "Majer", message: "Welcome!" }
+    { name: "Marcus", message: "Welcome!" }
 ]
 
 var typing = false
