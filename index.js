dc10c2507af1a3ce37a8712bb15dac6472ce82c9
marcus.chen <god850405@gmail.com>
2021-08-28 01:28:08 +0800

    微調

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 var fs = require('fs')
     // var https = require('https')
     // 如果不需要用 https 的話，要改成引用 http 喔
-var http = require('http')
-var socketio = require('socket.io')
-var app = require('express')();
+const express = require('express');
+const socketIO = require('socket.io');
 
 //https 的一些設定，如果不需要使用 ssl 加密連線的話，把內容註解掉就好
 var options = {
@@ -14,23 +13,15 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 //http & socket port
-var server = http.createServer(app); //option
-server.listen(PORT)
-var io = socketio(server, {
+var server = express()
+    .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
+var io = socketIO(server, {
     cors: {
         origin: "*",
         credentials: true
     }
 });
-app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-    //  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-// //用 api 方式取得
-// app.get('/', function(req, res) {
-//     res.sendFile(INDEX);
-//     // let messages = 'hellow world'
-//     // res.send(messages);
-// })
 
 var messages = [
     { name: "Marcus", message: "Welcome!" }
