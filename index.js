85445e1fdd90fef14e737ae7d85c51d01a380ff5
marcus.chen <god850405@gmail.com>
2022-06-06 23:13:22 +0800

    修改:新增 TYPE、TIME

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ var io = socketIO(server, {
 });
 
 var messages = [
-    { name: "Marcus", message: "Welcome!" }
+    {
+        name: "System",
+        message: "Welcome!" ,
+        type:'text',
+        time:ToDateTime(new Date())
+    }
 ]
 
 var typing = false
@@ -43,3 +48,24 @@ io.on('connection', function(socket) {
         }, 3000)
     })
 })
+
+function ToDateTime(date,separate=`-`){
+    const result = new Date(date);
+    const _yyyy = result.getFullYear();
+    const _MM = result.getMonth()+1;const MM = PadLeft(_MM,2,'0');
+    const _dd = result.getDate();const dd = PadLeft(_dd,2,'0');
+    const _hh = result.getHours();const hh = PadLeft(_hh,2,'0');
+    const _mm = result.getMinutes();const mm = PadLeft(_mm,2,'0');
+    const _ss = result.getSeconds();const ss = PadLeft(_ss,2,'0');
+    return `${_yyyy}${separate}${MM}${separate}${dd} ${hh}:${mm}:${ss}`;
+}
+
+function PadLeft(self,n , str){
+    if (typeof(n) !=='number' || isNaN(n)) return '';
+    let selfStr = `${self}`;
+    let leftStr = '';
+    for (let i =0 ; i<5 ;i++){
+        leftStr += str;
+    }
+    return (leftStr + selfStr).slice(-n);
+}
